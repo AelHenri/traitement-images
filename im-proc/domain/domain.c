05fe3bd33e6a3ad3ce0513cce3e20c3ac3c03b3b
GANGLOFF Nolwenn <nolwenn.gangloff@gmail.com>
2016-01-25 10:17:53 +0100

    lkh

--- im-proc/domain/domain.c
+++ im-proc/domain/domain.c
@@ -1,14 +1,19 @@
 #include <math.h>
 #include "domain.h"
 
-unsigned short 
+unsigned short
 bilinear_interpolation(float x, float y, pnm ims, int c)
 {
-  (void)x;
-  (void)y;
-  (void)ims;
-  (void)c;
+  int i = floor(x);
+  int j = floor(y);
+  
+  float dx = x - j;
+  float dy = y - i;
 
-  return 0;
+  unsigned short *q = pnm_get_image(ims);
+
+  short inter = (1-dx)*(1-dy)* q[c+pnm_offset(ims, i, j)] + dx*(1-dy)* q[c+pnm_offset(ims, i, j+1)] + (1-dx)*dy* q[c+pnm_offset(ims, i+1, j)] + dx*dy* q[c+pnm_offset(ims, i+1, j+1)];
+
+  return inter;
 }
 
