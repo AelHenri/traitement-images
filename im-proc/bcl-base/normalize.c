f3da7570036348ad1676c6acdcb832380342ca13
Henri <t.henri.al@gmail.com>
2016-01-25 10:32:19 +0100

    normalize et flip

--- im-proc/bcl-base/normalize.c
+++ im-proc/bcl-base/normalize.c
@@ -11,14 +11,16 @@ int max_value(unsigned short * img, int rows, int cols, int num) {
   
   int max = -1;
   int i,j;
+  unsigned short *p;
+  p = img;
   
   for(i=0;i<rows;i++){
 	  for(j=0;j<cols;j++){
 		  
-		  if(img[num]>max){
-			  max=img[num];
+		  if(p[num]>max){
+			  max=p[num];
 		  }
-		  img=img+3;
+		  p=p+3;
 	  }
   }
   return max;
@@ -28,21 +30,30 @@ int min_value(unsigned short * img, int rows, int cols, int num) {
   
   int min = 256;
   int i,j;
+  unsigned short *p;
+  p = img;
   
   for(i=0;i<rows;i++){
 	  for(j=0;j<cols;j++){
 		  
-		  if(img[num]<min){
-			  min=img[num];
+		  if(p[num]<min){
+			  min=p[num];
 		  }
-		  img=img+3;
+		  p=p+3;
 	  }
   }
   return min;
 }
 
-unsigned int normalize(int i, int j, int max, int min, int maxi, int mini) {
-		
+unsigned short normalize(unsigned int pixel, int max, int min, int maxi, int mini) {
+  unsigned int res;
+  float tmp1,tmp2,tmp3;;
+  
+  tmp1 = (float) (max-min) / (maxi-mini);
+  tmp2 = tmp1*pixel;
+  tmp3 = ((min*maxi)-(max*mini))/(maxi-mini);
+  res = tmp2 + tmp3;
+	return res;
 }
 
 void  
@@ -50,8 +61,8 @@ process(int min, int max, char* ims_name,char* imd_name){
   
   pnm ims = pnm_load(ims_name);
 
-  int rows = pnm_get_width(ims);
-  int cols = pnm_get_height(ims);
+  int cols = pnm_get_width(ims);
+  int rows = pnm_get_height(ims);
   
   pnm imd = pnm_new(cols, rows, PnmRawPpm);
 
@@ -59,16 +70,27 @@ process(int min, int max, char* ims_name,char* imd_name){
   unsigned short *pd = pnm_get_image(imd);
   
   int i,j;
-  unsigned short *image = pnm_get_image(ims);
+  int maxi[3], mini[3];
 
-  printf("%d\n", max_value(ps, rows, cols, 2));
+
+  //unsigned short *image = pnm_get_image(ims);
+  maxi[0] = max_value(ps, rows, cols, 0);
+  maxi[1] = max_value(ps, rows, cols, 1);
+  maxi[2] = max_value(ps, rows, cols, 2);
+  mini[0] = min_value(ps, rows, cols, 0);
+  mini[1] = min_value(ps, rows, cols, 1);
+  mini[2] = min_value(ps, rows, cols, 2);
+
+  printf("maxi1: %d, maxi2: %d, maxi3: %d\n",maxi[0],maxi[1],maxi[2] );
+  printf("mini1: %d, mini2: %d, mini3: %d\n",mini[0],mini[1],mini[2] );
   for(i=0;i<rows;i++){
 	  for(j=0;j<cols;j++){
-		  ps = image + pnm_offset(ims,i,j);
 		  for(int c=0; c<3; c++){
-			
-		  }
-	  }
+        *pd = normalize(*ps, max, min, maxi[c], mini[c]);
+        pd++;
+        ps++;
+      }
+    }
   }
   
   pnm_save(imd, PnmRawPpm, imd_name);
