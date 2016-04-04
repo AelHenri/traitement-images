c74c82bac0645e94d0951bf23101c3a602d3a18c
GANGLOFF Nolwenn <nolwenn.gangloff@gmail.com>
2016-04-04 14:18:25 +0200

    ajout fourier

---
+++ fourier/test-fft.c
@@ -0,0 +1,102 @@
+/**
+ * @file test-fft.c
+ * @brief test the behaviors of functions in fft module
+ *
+ */
+#include <float.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include "fft.h"
+
+/**
+ * @brief test the forward and backward functions
+ * @param pnm ims, the source image
+ * @param char* name, the image file name
+ */
+static void
+test_for_backward(pnm ims, char* name)
+{
+  fprintf(stderr, "test_for_backward: ");
+  (void)ims;
+  (void)name;
+  fprintf(stderr, "OK\n");
+}
+
+
+/**
+ * @brief test image reconstruction from of amplitude and phase spectrum
+ * @param pnm ims: the source image
+ * @param char *name: the image file name
+ */
+static void
+test_reconstruction(pnm ims, char* name)
+{
+  fprintf(stderr, "test_reconstruction: ");
+  (void)ims;
+  (void)name;
+  fprintf(stderr, "OK\n");
+}
+
+
+/**
+ * @brief test construction of amplitude and phase images in ppm files
+ * @param pnm ims, the source image
+ * @param char* name, the image file name
+ */
+static void
+test_display(pnm ims, char* name)
+{
+  fprintf(stderr, "test_display: ");
+  (void)ims;
+  (void)name;
+  fprintf(stderr, "OK\n");
+}
+
+
+/**
+ * @brief test the modification of amplitude by adding a periodic functions
+          on both vertical and horizontal axis, and 
+ *        construct output images
+ * @param pnm ims, the source image
+ * @param char* name, the image file name
+ */
+static void
+test_add_frequencies(pnm ims, char* name)
+{
+  fprintf(stderr, "test_add_frequencies: ");
+  (void)ims;
+  (void)name;
+  fprintf(stderr, "OK\n");
+}
+
+static void 
+run(pnm ims, char* name)
+{
+  test_for_backward(ims, name);
+  test_reconstruction(ims, name);
+  test_display(ims, name);
+  test_add_frequencies(ims, name);
+}
+
+
+void 
+usage (char *s)
+{
+  fprintf(stderr, "Usage: %s <ims> \n", s);
+  exit(EXIT_FAILURE);
+}
+
+int 
+main(int argc, char *argv[])
+{
+  if (argc != 2)
+    usage(argv[0]);
+  pnm ims = pnm_load(argv[1]);
+  run(ims, argv[1]);
+
+  return EXIT_SUCCESS;
+}
