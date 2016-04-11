346b81571946f48407fd70e67185056944f4ebc3
GANGLOFF Nolwenn <nolwenn.gangloff@gmail.com>
2016-04-11 16:50:27 +0200

    fin padding

--- im-proc/zoom/padding.c
+++ im-proc/zoom/padding.c
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <bcl.h>
 #include "fft.h"
+#include <math.h>
 
 
  void process(int factor, char* ims_name,char* imd_name){
@@ -21,87 +22,57 @@
 
   pnm imd = pnm_new(new_cols, new_rows, PnmRawPpm);
 
-  unsigned short *ps = pnm_get_image(ims);
-  unsigned short *pd = pnm_get_image(imd);
+  unsigned short *ps_one_c = malloc(rows*cols*sizeof(unsigned short));
+  for(int i=0;i<rows;i++)
+      for(int j=0;j<cols;j++){
+		*ps_one_c = pnm_get_component(ims,i,j,1);
+		ps_one_c++;
+	}
+  ps_one_c -= cols*rows;
 
   fftw_complex *fftw_for;
   fftw_complex *fftw_center = (fftw_complex*) fftw_malloc(new_rows*new_cols*sizeof(fftw_complex));;
 
-  fftw_for = forward(rows, cols, ps);
-
+  fftw_for = forward(rows, cols, ps_one_c);
+  
   int mn_rows = new_rows/2;
   int mn_cols = new_cols/2;
   
   int m_rows = rows/2;
   int m_cols = cols/2;
   
-  printf("plop1\n");
-  
   for (int i = 0; i < new_rows; ++i)
   {
     for (int j = 0; j < new_cols; ++j)
     {
-	  if((i>=(mn_rows-m_rows))&&(i<=(mn_rows+m_rows))&&(j>=(mn_cols-m_cols))&&(j<=(mn_cols+m_cols))){
+	  if((i>=(mn_rows-m_rows))&&(i<(mn_rows+m_rows))&&(j>=(mn_cols-m_cols))&&(j<(mn_cols+m_cols))){
 		  *fftw_center = *fftw_for;
-		  fftw_center++;     
-		  fftw_for++;       
+		  fftw_for++;
 	  }
 	  else{
 		  *fftw_center = 0;
-		  fftw_center++;     	 
 	  }
+	  fftw_center++;     	 
     }
   }
+    
   fftw_center -= new_rows*new_cols;
-  
-
-  pnm step = pnm_new(new_cols, new_rows, PnmRawPpm);
-  unsigned short *st = pnm_get_image(step);
-  
-   for (int i = 0; i < new_rows; ++i)
-   {
-    for (int j = 0; j < new_cols; ++j)
-    {
-	  for (int k = 0; k < 3 ; ++k){
-		*st = creal(*fftw_center);
-	    st++;
-	  }
-	  fftw_center++;
-    }
-  } 
-  fftw_center -= new_rows*new_cols;
-  st -= new_rows*new_cols*3;
-
-  pnm_save(step, PnmRawPpm, "step.ppm");
-
-  
+  fftw_for -= rows*cols;
  
-
-  fftw_center -= new_rows*new_cols;
   unsigned short *out = backward(new_rows, new_cols, fftw_center,factor);
-  
-  
+    
     for(int i=0;i<new_rows;i++){
       for(int j=0;j<new_cols;j++){
-
         for(int c=0; c<3; c++){
-          *pd = *out;
-          pd++;
-          out++;
+          pnm_set_component(imd,i,j,c,*out);
         }
-        
+        out++;
       }
     }
-    printf("plop3.5\n");
-
-    pd -= 3*new_cols*new_rows;
-    out -= 3*new_cols*new_rows;
-  
-    printf("plop4\n");
-    
     pnm_save(imd, PnmRawPpm, imd_name);
-
-    
+	free(ps_one_c);
+	pnm_free(imd);
+	pnm_free(ims);
 }
 
 void 
